bd06d8e5ebfa8cf5737ab1b37c66653b9dd99c86
RiceFaceDuck <bentsbac@gmail.com>
2026-04-24 10:17:39 +0700

    อัปเดตงาน 24 เม.ย. 2026

--- dh-frontend/src/hooks/useCart.js
+++ dh-frontend/src/hooks/useCart.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
-import { CartContext } from '../contexts/CartContext';
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext"; // [UPDATE] แก้ไขชื่อโฟลเดอร์จาก contexts เป็น context
 
 export const useCart = () => useContext(CartContext);
